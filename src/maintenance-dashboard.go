a30b51c2d9332c5f9db8fcbd606503f50a36b56e
Csaba Fabian <csaba.fabian@t-systems.com>
2022-07-15 13:04:37 +0000

    add api call to newreleases project list, accept json

--- src/maintenance-dashboard.go
+++ src/maintenance-dashboard.go
@@ -1,32 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "os"
+        "fmt"
+        "io"
+        "log"
+        "net/http"
+        // "encoding/json"
+        "os"
+
+        // "github.com/prometheus/client_golang/prometheus"
+        // "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type Page struct {
-    Title string
-    Body  []byte
-}
+func makeApiCall() {
+        NewReleasesApiKey := os.Args[1]
+        client := http.Client{}
+        url := "https://api.newreleases.io/v1/projects"
+        req , err := http.NewRequest("GET", url, nil)
+        if err != nil {
+                log.Fatalln(err)
+        }
+        // req.Header.Set("X-Key", NewReleasesApiKey)
 
-func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return os.WriteFile(filename, p.Body, 0600)
-}
+        req.Header = http.Header{
+                "Content-Type": {"application/json"},
+                "X-Key": {NewReleasesApiKey},
+        }
 
-func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := os.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
+        res, err := client.Do(req)
+        if err != nil {
+                log.Fatalln(err)
+        }
+        body, err := io.ReadAll(res.Body)
+        res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", body)
 }
 
 func main() {
-    p1 := &Page{Title: "DevSecOps", Body: []byte("This is the page we want to have information on the currently deployed and the latest releases of all our applications.")}
-    p1.save()
-    p2, _ := loadPage("DevSecOps")
-    fmt.Println(string(p2.Body))
+        makeApiCall()
+        // http.Handle("/metrics", promhttp.Handler())
+        // log.Fatal(http.ListenAndServe(":2112", nil))
 }
